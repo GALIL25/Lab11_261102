88ca88d71edfcf4413e8ccb90120d36dbc2f6e8b
GARLIL25 <36127@dara.ac.th>
2026-01-05 20:37:20 +0700

    lab11_1.cpp random

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,27 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future." << endl;
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int r = rand() % 9;
+    string grade;
+    if (r == 0) grade = "A";
+    else if (r == 1) grade = "B+";
+    else if (r == 2) grade = "B";
+    else if (r == 3) grade = "C+";
+    else if (r == 4) grade = "C";
+    else if (r == 5) grade = "D+";
+    else if (r == 6) grade = "D";
+    else if (r == 7) grade = "F";
+    else grade = "W";
+
+    cout << "You will get " << grade << " in this 261102." << endl;
+
+    return 0;
+}
